b1ca92cce882cc4f9e299a241bffe55ad56f1e2d
Norgius <dmitry.schadrin21@yandex.ru>
2022-11-14 20:04:10 +0300

    Via AdminInline show owners on page Flat

--- property/admin.py
+++ property/admin.py
@@ -3,13 +3,25 @@ from django.contrib import admin
 from .models import Flat, Complaint, Owner
 
 
+class OwnerInline(admin.TabularInline):
+    model = Flat.owners.through
+    raw_id_fields = ('owner', )
+
+
 class FlatAdmin(admin.ModelAdmin):
-    search_fields = ('town', 'address', 'owner')
+    search_fields = ('town', 'address',)
     readonly_fields = ['created_at']
-    list_display = ('address', 'price', 'new_building', 'construction_year', 'town', 'owners_phonenumber', 'owner_pure_phone')
+    list_display = (
+        'address',
+        'price',
+        'new_building',
+        'construction_year',
+        'town',)
     list_editable = ('new_building', )
     list_filter = ('new_building', 'rooms_number', 'has_balcony')
     raw_id_fields = ('liked_by',)
+    inlines = [OwnerInline, ]
+
 
 class ComplaintAdmin(admin.ModelAdmin):
     raw_id_fields = ('person', 'flat',)
