2147ec7de7adda3f1e4293a1b7406ac7521ea343
Norgius <dmitry.schadrin21@yandex.ru>
2022-11-17 20:14:25 +0300

    Reduce requests number

--- property/migrations/0004_auto_20221111_2110.py
+++ property/migrations/0004_auto_20221111_2110.py
@@ -5,20 +5,13 @@ from django.db import migrations
 
 def is_this_new_building(apps, schema_editor):
     Flat = apps.get_model('property', 'Flat')
-    for flat in Flat.objects.all():
-        if flat.construction_year >= 2015:
-            flat.new_building = True
-            flat.save()
-        else:
-            flat.new_building = False
-            flat.save()
+    Flat.objects.filter(construction_year__gte=2015).update(new_building=True)
+    Flat.objects.filter(construction_year__lt=2015).update(new_building=False)
 
 
 def move_backward(apps, schema_editor):
     Flat = apps.get_model('property', 'Flat')
-    for flat in Flat.objects.all():
-        flat.new_building = None
-        flat.save()
+    Flat.objects.all().update(new_building=None)
 
 
 class Migration(migrations.Migration):
